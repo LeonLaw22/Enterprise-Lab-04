c497eae2ff57e85a1fcc411e98ad83b993dd537c
LEON LAW (1603156) <1603156@rgu.ac.uk>
2020-03-04 12:19:30 +0000

    test

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ http.createServer(function (request, response) {
 	//Send the HTTP header
 	//HTTP Status: 200 : OK
 	//Contest Type: text/plain
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.writeHead(200, {
+		'Content-Type': 'text/html'
+		'Access-Control-Allow-origin' : '*'
+	});
+	var readStream = fs.createreadStream(__dirname + 'index.html');
+	//send a message
+	readStream.pipe(response);
 	
 	//Send the response body as "Hello World"
 	response.and('Hello World\n');
-})app.listen(3000, function () {
+})listen(PORT);
 	console.log('Example app listening on port 3000!');
-});
+
 
 //Console will print the message 
+var http = require("http");
+fs = require('fs');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log('Our app is tunning on port ${ PORT }');
